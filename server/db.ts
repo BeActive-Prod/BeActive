51bf58cdca621f6bad87de1c6a32aabaea7a517c
Simon <sdesmaison@student.ethz.ch>
2026-01-07 00:46:14 +0100

    Adding data folder with db file inside so it can be consistant on docker compose

--- server/db.ts
+++ server/db.ts
@@ -2,7 +2,11 @@ import Database from 'better-sqlite3';
 import path from 'path';
 import { runMigrations } from './migrations';
 
-const dbPath = path.join(process.cwd(), 'beactive.db');
+const dbPath = path.join(process.cwd(), './data/beactive.db');
+// Ensure the data directory exists, if not create it
+if (!require('fs').existsSync(path.dirname(dbPath))) {
+  require('fs').mkdirSync(path.dirname(dbPath), { recursive: true });
+}
 const db = new Database(dbPath);
 
 // Enable foreign keys
